ca55f34385cb0a6b414a469f3f984db801549bb8
gabriele <gabriele.manfredi@hotmail.it>
2017-11-13 18:33:38 +0100

    Implemented PropertiesBeanFactory
    	modified:   src/main/java/it/madlabs/springtalk/utils/PropertiesBeanFactory.java
    	modified:   src/main/resources/configuration.properties

--- src/main/java/it/madlabs/springtalk/utils/PropertiesBeanFactory.java
+++ src/main/java/it/madlabs/springtalk/utils/PropertiesBeanFactory.java
@@ -4,6 +4,8 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -36,16 +38,23 @@ public class PropertiesBeanFactory implements BeanFactory {
             if (rawRefConfig.startsWith("ref.")){
                 try {
                     String refConfig = rawRefConfig.replace("ref.", "");
+                    String firstBeanName = refConfig.split("\\.")[0];
+                    String setterMethodName = refConfig.split("\\.")[1];
+                    String secondBeanName = properties.getProperty(rawRefConfig);
 
-                    String beanClassName = properties.getProperty(rawRefConfig);
-                    Class<?> beanClass = Class.forName(beanClassName);
-                    Object bean = beanClass.newInstance();
-                    applicationContext.put(refConfig, bean);
-                } catch (ClassNotFoundException e) {
+
+                    Object firstBean = applicationContext.get(firstBeanName);
+                    Object secondBean = applicationContext.get(secondBeanName);
+
+                    Method setterMethod = firstBean.getClass().getMethod(setterMethodName, secondBean.getClass().getInterfaces()[0]);
+
+                    setterMethod.invoke(firstBean, secondBean);
+
+                } catch (NoSuchMethodException e) {
                     e.printStackTrace();
                 } catch (IllegalAccessException e) {
                     e.printStackTrace();
-                } catch (InstantiationException e) {
+                } catch (InvocationTargetException e) {
                     e.printStackTrace();
                 }
             }
@@ -56,7 +65,7 @@ public class PropertiesBeanFactory implements BeanFactory {
     private Properties loadProperties(String file) {
         Properties properties = new Properties();
         try {
-            InputStream input = new FileInputStream(file);
+            InputStream input = Thread.currentThread().getContextClassLoader().getResourceAsStream(file);
             properties.load(input);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -67,6 +76,6 @@ public class PropertiesBeanFactory implements BeanFactory {
     }
 
     public Object getBean(String beanName) {
-        return null;
+        return applicationContext.get(beanName);
     }
 }
