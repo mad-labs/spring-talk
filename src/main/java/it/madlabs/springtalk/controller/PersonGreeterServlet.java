5853b17c0586c25e1ac3ddd08879f1ff29895659
gabriele <gabriele.manfredi@hotmail.it>
2017-11-17 15:57:38 +0100

    Added WebApp

--- src/main/java/it/madlabs/springtalk/controller/PersonGreeterServlet.java
+++ src/main/java/it/madlabs/springtalk/controller/PersonGreeterServlet.java
@@ -1,8 +1,8 @@
 package it.madlabs.springtalk.controller;
 
 import it.madlabs.springtalk.business.services.PersonGreeterService;
-import it.madlabs.springtalk.utils.BeanFactory;
-import it.madlabs.springtalk.utils.PropertiesBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -16,9 +16,11 @@ public class PersonGreeterServlet
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        BeanFactory beanFactory = new PropertiesBeanFactory("configuration.properties");
+//        BeanFactory beanFactory = new PropertiesBeanFactory("configuration.properties");
+//        PersonGreeterService personGreeterService = (PersonGreeterService) beanFactory.getBean("personGreeterService");
 
-        PersonGreeterService personGreeterService = (PersonGreeterService) beanFactory.getBean("personGreeterService");
+        ApplicationContext context = new ClassPathXmlApplicationContext("application-context.xml");
+        PersonGreeterService personGreeterService = (PersonGreeterService) context.getBean("personGreeterService");
 
         String message = personGreeterService.composeGreetingToRandomPerson();
         req.setAttribute("message", message);
