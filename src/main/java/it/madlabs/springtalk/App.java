d71323993f815cf01b0bbe7d089a9c79c4ca9693
Gabriele <gabriele.manfredi.999@gmail.com>
2017-11-19 22:57:20 +0100

    modified:   src/main/java/it/madlabs/springtalk/App.java 	new file:   src/main/resources/application-context.xml

--- src/main/java/it/madlabs/springtalk/App.java
+++ src/main/java/it/madlabs/springtalk/App.java
@@ -4,6 +4,8 @@ import it.madlabs.springtalk.business.services.PersonGreeterService;
 import it.madlabs.springtalk.business.services.impl.PersonGreeterServiceImpl;
 import it.madlabs.springtalk.utils.BeanFactory;
 import it.madlabs.springtalk.utils.PropertiesBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Hello world!
@@ -13,8 +15,8 @@ public class App {
     public static void main( String[] args ) {
         System.out.println("\n *** WELCOME TO GREAT PERSONS GREETER *** \n");
 
+//        ApplicationContext context = new ClassPathXmlApplicationContext("application-context.xml");
         BeanFactory beanFactory = new PropertiesBeanFactory("configuration.properties");
-
         PersonGreeterService personGreeterService = (PersonGreeterService) beanFactory.getBean("personGreeterService");
 
         String worldHello = personGreeterService.composeGreetingToTheWorld();
